a79b86f6e123d738c05fd53b86b214031cfdc26b
Travis Spomer <16262858+TravisSpomer@users.noreply.github.com>
2022-04-12 11:44:13 -0700

    Add project files.

---
+++ Aggregate.cs
@@ -0,0 +1,60 @@
+﻿namespace Aggregate
+{
+	public sealed class Aggregate<TItem, TValue> where TItem : class
+	{
+		private readonly IList<TItem> _items;
+		private readonly Func<TItem, TValue?> _getter;
+		private readonly Action<TItem, TValue?> _setter;
+		private readonly TValue? _fallback = default;
+
+		public Aggregate(IList<TItem> items, Func<TItem, TValue?> getter, Action<TItem, TValue?> setter, TValue? fallback = default)
+		{
+			_items = items;
+			_getter = getter;
+			_setter = setter;
+			_fallback = fallback;
+		}
+
+		private (bool, TValue?) CheckEquality()
+		{
+			if (_items.Count == 0)
+			{
+				return (true, _fallback);
+			}
+
+			TValue? first = _getter(_items.First());
+			bool equal;
+			if (first != null)
+				equal = _items.Skip(1).All(value => first.Equals(_getter(value)));
+			else
+				equal = _items.Skip(1).All(value => _getter(value) == null);
+
+			return (equal, equal ? first : _fallback);
+		}
+
+		public bool Equal
+		{
+			get
+			{
+				(bool equal, _) = CheckEquality();
+				return equal;
+			}
+		}
+
+		public TValue? Value
+		{
+			get
+			{
+				(_, TValue? value) = CheckEquality();
+				return value;
+			}
+			set
+			{
+				foreach (TItem item in _items)
+				{
+					_setter(item, value);
+				}
+			}
+		}
+	}
+}
---
+++ Pages/Index.razor.cs
@@ -0,0 +1,15 @@
+﻿namespace Aggregate.Pages
+{
+	public partial class Index
+	{
+		private readonly Aggregate<Person, string> AggregatedName;
+		private readonly Aggregate<Person, string> AggregatedCountry;
+
+		public Index()
+		{
+			// These have to be initialized here because they depend on "people", and the order that initializers happen in is not guaranteed.
+			AggregatedName = new(people, person => person.Name, (person, value) => person.Name = value ?? string.Empty, "(Multiple names)");
+			AggregatedCountry = new(people, person => person.Country, (person, value) => person.Country = value ?? string.Empty, string.Empty);
+		}
+	}
+}
